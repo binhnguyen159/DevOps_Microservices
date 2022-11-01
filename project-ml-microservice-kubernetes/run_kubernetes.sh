439a6be67ad7489fc64b2e580817f7b59ba9631b
EC2 Default User <ec2-user@ip-172-31-84-125.ec2.internal>
2022-11-01 08:18:32 +0000

    Task 7: [Important] Delete Cluster

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=binhnguyen058/prediction-app:latest
+echo "Docker ID and Image: $dockerpath"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run prediction-app --image=$dockerpath --port=8000 --labels app=binhnguyen058
 
 
 # Step 3:
 # List kubernetes pods
+sleep 100
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward prediction-app 8000:8000
